ed91be663f5d2437d7d077450c2d5031166e2ac0
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-11-05 17:58:44 +0100

    Install cloudinary

--- server.js
+++ server.js
@@ -4,19 +4,17 @@ const pg = require("pg");
 const express = require('express');
 const app = express();
  
-app.get('/', (req, res)=> {
+app.get('/', async (req, res)=> {
 	const pool = new pg.Pool({ connectionString: DATABASE_URL });
 	
-	return pool.query('SELECT NOW()', (err, result) => {
-		if(err) {
-			console.log("not able to get connection "+ err);
-			res.status(400).send(err);
-		}
-
-		res.status(200).json(result);
-
-		pool.end();
+		// you can also use async/await
+	const result = await pool.query('SELECT NOW()').catch((err)=> {
+		console.log("not able to get connection " + err);
+		res.status(400).send(err);
 	});
+	res.status(200).json(result);
+
+	await pool.end();
 
 	const client = new pg.Client({ connectionString: DATABASE_URL });
 	client.connect();

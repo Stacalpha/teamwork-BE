d6101831d49f910faa65e206f27965f83b2e1e7a
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-11-12 15:37:29 +0100

    create initRoutes

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+const welcome = (req, res)=> {
+	res.status(200).json({
+		success: true,
+		message: "You've reached the root route. React app is served here."
+	});
+}
+
+const initRoutes = (app)=> {
+	app.get('/', welcome);
+
+	app.post('/auth/create-user', createNewUser);
+};
+
+module.exports = initRoutes;
--- server.js
+++ server.js
@@ -5,14 +5,5 @@ const createNewUser = require('./controllers/user-controllers/user-controller');
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res)=> {
-	res.status(200).json({
-		success: true,
-		message: "You've reached the root route. React app is served here."
-	});
-});
-
-app.post('/auth/create-user', createNewUser);
  
 app.listen(4000, ()=>	console.log('Server is running.. on Port 4000'));

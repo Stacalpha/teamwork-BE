e443b65f088c6ac6781d97bccc8e9dca7e9ac187
Feranmi Akinlade <feranmiakinlade@gmail.com>
2019-11-15 08:29:39 -0800

    Test passing

--- services/user-services/validate-user-data.js
+++ services/user-services/validate-user-data.js
@@ -1,20 +1,27 @@
 const userDataIsValid = (userData) => {
-  const {
-    firstName, lastName, email, password, gender, jobRole, department, address,
-  } = userData;
+  try {
+    const {
+      firstName, lastName, email, password, gender, jobRole, department, address,
+    } = userData;
 
-  if (
-    firstName.length > 1
-    && lastName.length > 1
-    && email.length > 5 && email.contains('@') && email.contains('.')
-    && password.length > 6
-    && (!gender || gender === 'Male' || gender === 'Female')
-    && jobRole
-    && department
-    && address
-  ) return true;
+    console.log(userData);
 
-  return false;
+    if (
+      firstName.length > 1
+      && lastName.length > 1
+      && email.length > 5 && email.includes('@') && email.includes('.')
+      && password.length > 5
+      && (!gender || gender === 'Male' || gender === 'Female')
+      && jobRole
+      && department
+      && address
+    ) return true;
+
+    return false;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 };
 
 module.exports = userDataIsValid;

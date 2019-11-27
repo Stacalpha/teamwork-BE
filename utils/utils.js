9c06d6d90d55fcb5dd4e682a48725876e8465eda
Akinlade Feranmi <30551780+Stacalpha@users.noreply.github.com>
2019-11-27 13:58:28 +0000

    Export parseJsonBody from utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 const responseSender = require('./response-sender');
 const tokenChecker = require('./token-checker');
+const parseJsonBody = require('./parse-json-body.js');
 
-module.exports = { responseSender, tokenChecker };
+module.exports = { responseSender, tokenChecker, parseJsonBody };

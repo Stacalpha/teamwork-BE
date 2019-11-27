798b26fe404893a20b355cf21ada74a48184b71b
Akinlade Feranmi <30551780+Stacalpha@users.noreply.github.com>
2019-11-27 14:02:08 +0000

    Log content type header to console

--- utils/parse-json-body.js
+++ utils/parse-json-body.js
@@ -1,5 +1,7 @@
 const parseJsonBody = (express) => {
   const checkHeaderAndParse = (req, res, next) => {
+    console.log(req.headers['content-type']);
+
     if (req.headers['content-type'] !== 'multipart/formdata') {
       const parser = express.json();
       return parser(req, res, next);
